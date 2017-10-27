fae9a1b95774eae4dea9bc2e00018c93b8caa7f3
Kingjv <jvongkhamsay@hotmail.com>
2017-10-27 15:54:22 -0500

    init commit

---
+++ src/app/route/home/home.component.ts
@@ -0,0 +1,54 @@
+import { HttpModule, Http } from '@angular/http';
+import { Component, OnInit } from "@angular/core";
+import 'whatwg-fetch'
+import { Observable } from "rxjs/Observable";
+@Component({
+    selector: "tyn-home",
+    templateUrl: './home.component.html',
+    styleUrls: ["./home.component.scss"]
+})
+export class HomeComponent implements OnInit {
+    x: any;
+    z: Observable<any> = new Observable;
+    constructor(
+        private http: Http
+    ) {
+
+    }
+
+    ngOnInit() {
+
+    }
+
+    getCards() {
+        this.getCard().subscribe((response) => {
+            debugger;
+            this.x = response.cards[0];
+        });
+    }
+
+    getCard(): Observable<any> {
+        let y: any;
+        const endpoint = 'https://api.magicthegathering.io/v1/cards';
+
+        return this.http
+        .get(endpoint)//, {search: searchParams})
+        .map((res ) => {
+            return res.json()
+        })
+        // fetch('https://api.magicthegathering.io/v1/cards')
+        //     .then(function (response) {
+        //         debugger
+        //         return response.json()
+        //     }).then(function (json) {
+        //         console.log('parsed json', json)
+        //         y = json;
+        //         return y;
+        //     }).catch(function (ex) {
+        //         console.log('parsing failed', ex)
+        //         y = ex;
+        //     })
+    }
+
+
+}
