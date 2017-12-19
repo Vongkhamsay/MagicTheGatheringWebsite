3817cabfffee4eab14026dda16a156ea3ecd8b18
Kingjv <jvongkhamsay@hotmail.com>
2017-12-18 19:47:32 -0600

    Checkin. Potential tab layout? Would need to create a component for each tab section (maybe)

--- src/app/shared/details-tabs/details-tabs.component.ts
+++ src/app/shared/details-tabs/details-tabs.component.ts
@@ -1,15 +1,23 @@
+import { CardDetailStateService } from './../card-detail-state/card-detail-state.service';
+import { Card } from './../models/card';
 import { Component, OnInit, Inject } from "@angular/core";
 
 @Component({
-    selector: "details-tabs",    
+    selector: "details-tabs",
     templateUrl: './details-tabs.component.html',
     styleUrls: ['./details-tabs.component.scss']
 })
 export class DetailsTabsComponent implements OnInit {
 
-    constructor(
-    ) {
+    card: Card;
 
+    constructor(
+        private cardDetailState: CardDetailStateService
+
+    ) {
+        this.cardDetailState.watch.subscribe((response) => {
+            this.card = response;
+        })
     }
 
     ngOnInit() {
